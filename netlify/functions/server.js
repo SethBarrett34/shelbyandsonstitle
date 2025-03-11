406f6164a57056538653f05fbb1677fadbfbd94c
SethBarrett34 <105762664+SethBarrett34@users.noreply.github.com>
2025-03-11 16:36:08 -0600

    Update Netlify server adapter to use express and serverless-http

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,23 @@
-import { createRequestHandler } from "@remix-run/netlify";
-import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from "@remix-run/express";
+import * as build from "../../build/index.js";
+import express from "express";
+import { installGlobals } from "@remix-run/node";
+import serverless from "serverless-http";
 
-export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
+installGlobals();
+
+const app = express();
+
+app.use(express.static("public", { maxAge: "1h" }));
+app.use(express.static("public/build", { immutable: true, maxAge: "1y" }));
+
+app.all(
+  "*",
+  createRequestHandler({
+    build,
+    mode: process.env.NODE_ENV,
+  })
+);
+
+// Export the serverless function for Netlify
+export const handler = serverless(app);
