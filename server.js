6429b4ee797cd8cfd782caf0cb8f5a6bd034c6ce
SethBarrett34 <105762664+SethBarrett34@users.noreply.github.com>
2025-03-11 16:36:51 -0600

    Simplify server.js for easier development

--- server.js
+++ server.js
@@ -1,41 +1,26 @@
-import { createRequestHandler } from "@remix-run/express";
 import { installGlobals } from "@remix-run/node";
 import express from "express";
+import { createRequestHandler } from "@remix-run/express";
 
 installGlobals();
 
-const viteDevServer =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : await import("vite").then((vite) =>
-        vite.createServer({
-          server: { middlewareMode: true },
-        })
-      );
-
 const app = express();
 
-// handle asset requests
-if (viteDevServer) {
-  app.use(viteDevServer.middlewares);
-} else {
-  app.use(
-    "/build",
-    express.static("public/build", {
-      immutable: true,
-      maxAge: "1y",
-    })
-  );
-}
+// Handle asset requests
+app.use(
+  "/build",
+  express.static("public/build", { immutable: true, maxAge: "1y" })
+);
+
+// Handle public asset requests
 app.use(express.static("public", { maxAge: "1h" }));
 
-// handle SSR requests
+// Handle SSR requests
 app.all(
   "*",
   createRequestHandler({
-    build: viteDevServer
-      ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
-      : await import("./build/index.js"),
+    build: await import("./build/index.js"),
+    mode: process.env.NODE_ENV,
   })
 );
 
