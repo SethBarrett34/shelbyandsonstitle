a1f01ddaa5aed76a3dbbf10b597fc6467bae8800
SethBarrett34 <105762664+SethBarrett34@users.noreply.github.com>
2025-03-13 13:55:33 -0600

    Update server.js to handle static assets properly

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 import { installGlobals } from "@remix-run/node";
 import express from "express";
 import { createRequestHandler } from "@remix-run/express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 installGlobals();
 
+const BUILD_DIR = path.join(__dirname, "build");
+const PUBLIC_DIR = path.join(__dirname, "public");
+
+// Create an express app
 const app = express();
 
-// Handle asset requests
-app.use(
-  "/build",
-  express.static("public/build", { immutable: true, maxAge: "1y" })
-);
+// Serve public directory at the root path
+app.use(express.static(PUBLIC_DIR, { maxAge: "1h" }));
 
-// Handle public asset requests
-app.use(express.static("public", { maxAge: "1h" }));
+// Add any other static asset directories here
+// e.g. app.use(express.static("public/images"));
 
-// Handle SSR requests
+// Then handle client entries
 app.all(
   "*",
   createRequestHandler({
